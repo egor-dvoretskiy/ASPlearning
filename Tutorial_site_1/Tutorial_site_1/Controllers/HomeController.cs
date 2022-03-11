2d414e585c46a4f599a4222aecbb9c5500308c46
ergo lance <brainispower21@gmail.com>
2022-03-11 15:20:57 +0300

    Changes to run site in demo vrs.

---
+++ Tutorial_site_1/Tutorial_site_1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial_site_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Tutorial_site_1/Tutorial_site_1/Startup.cs
+++ Tutorial_site_1/Tutorial_site_1/Startup.cs
@@ -16,6 +16,9 @@ namespace Tutorial_site_1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews()
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
+                .AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +30,11 @@ namespace Tutorial_site_1
             }
 
             app.UseRouting();
+            app.UseStaticFiles(); // wwwroot files.
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
