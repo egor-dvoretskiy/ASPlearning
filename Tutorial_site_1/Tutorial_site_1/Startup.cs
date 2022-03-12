3e21204901baaee4845826018c278e66f0ce4416
ergo lance <brainispower21@gmail.com>
2022-03-12 12:25:33 +0300

    Add Configuration files.

--- Tutorial_site_1/Tutorial_site_1/Startup.cs
+++ Tutorial_site_1/Tutorial_site_1/Startup.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tutorial_site_1.Service;
 
 namespace Tutorial_site_1
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => this.Configuration = configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // connect config from appsetttings.json
+            this.Configuration.Bind("Project", new Config());
+
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0)
                 .AddSessionStateTempDataProvider();
