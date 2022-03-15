30ba46a97e45f88559b3ed2f56b5ed3e6e160be0
ergo lance <brainispower21@gmail.com>
2022-03-15 12:11:57 +0300

    Finished demo.

--- Tutorial_site_1/Tutorial_site_1/Controllers/HomeController.cs
+++ Tutorial_site_1/Tutorial_site_1/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial_site_1.Domain.Repositories;
 
 namespace Tutorial_site_1.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ Tutorial_site_1/Tutorial_site_1/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tutorial_site_1.Domain.Repositories;
+
+namespace Tutorial_site_1.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.ServiceItems.GetServiceItems()));
+        }
+    }
+}
